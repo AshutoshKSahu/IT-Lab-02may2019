ba58fcc07cd1bfa209bed938fd8984ec7ad4eb2d
Ashutosh Sahu <47266286+ashu21sahu@users.noreply.github.com>
2019-05-03 12:37:13 +0530

    Update voted.php

--- voted.php
+++ voted.php
@@ -1,3 +1,7 @@
+<html>
+<body>
+<div style="background-color:lightgreen;border:2px solid black;padding:10px;">
+<center>
 <?php
 include('config.php');
 session_start();
@@ -6,21 +10,23 @@ $sql4="select voted as 'vote' from login where uid='$uid'";
 $res=mysqli_query($link,$sql4);
 $row=mysqli_fetch_assoc($res);
 $row2=$row['vote'];
-
 if($row2==1)
 {
-echo "You have already voted<br>";
-echo "<input type=button onclick=location='login.html' value=Logout>";
+	echo "You have already voted<br>";
+	echo "<input type=button onclick=location='login.html' value=Logout>";
 }
 else
 {
-$val=$_POST['candi'];
-$sql2="update candidate set vcount=vcount+1 where cname='$val'";
-$result1=mysqli_query($link,$sql2);
-echo "You've voted successfully!";
-
-$sql3="update login set voted=1 where uid='$uid'";
-mysqli_query($link,$sql3);
-echo "<input type=button onclick=location='login.html' value=Logout>";
+	$val=$_POST['candi'];
+	$sql2="update candidate set vcount=vcount+1 where cname='$val'";
+	$result1=mysqli_query($link,$sql2);
+	echo "You've voted successfully!";
+	$sql3="update login set voted=1 where uid='$uid'";
+	mysqli_query($link,$sql3);
+	echo "<input type=button onclick=location='login.html' value=Logout>";
 }
 ?>
+</div>
+</center>
+</body>
+</html>
