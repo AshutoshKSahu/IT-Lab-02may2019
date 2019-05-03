01f3bfcc75c0d6249b44c7a143496a1e4d20e264
Ashutosh Sahu <47266286+ashu21sahu@users.noreply.github.com>
2019-05-03 12:34:52 +0530

    Update candivote.php

--- candivote.php
+++ candivote.php
@@ -1,27 +1,25 @@
 <html>
-<body><center>
+<body>
+<div style="background-color:lightgreen;border:2px solid black;padding:10px;">
+<center>
 <h1>Check votes</h1><br><br>
 <?php
 include('config.php');
 session_start();
 $uid=$_SESSION['uidd'];
-
 $sql="select vcount as 'vote' from candidate where cid='$uid'";
 $result=mysqli_query($link,$sql);
 $row=mysqli_fetch_assoc($result);
 $row2=$row['vote'];
-
 //while($row=mysqli_fetch_row($result))
 //{
 	//$cid=$row['cid'];
 	//$cname=$row['cname'];
 	//$vcount=$row['vcount'];
-	
 	$sql3="select count(cid) as 'total' from declaree";
 	$res=mysqli_query($link,$sql3);
 	$row3=mysqli_fetch_assoc($res);
 	$r=$row3['total'];
-	
 	if($r==1)
 	{ 
 		echo "Your total vote count is:$row2<br>";
@@ -35,5 +33,6 @@ $row2=$row['vote'];
 <br><br>
 <input type=button value=Logout onclick="location='login.html'">
 </center>
+</div>
 </body>
 </html>
