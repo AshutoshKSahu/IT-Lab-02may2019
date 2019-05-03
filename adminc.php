eb38dcea9a60c7c2b2ae59be6e4d71996683730f
Ashutosh Sahu <47266286+ashu21sahu@users.noreply.github.com>
2019-05-03 12:33:27 +0530

    Update adminc.php

--- adminc.php
+++ adminc.php
@@ -1,3 +1,5 @@
+<div style="background-color:lightpink;border:2px solid black;padding:10px;">
+<center>
 <?php
 include('config.php');
 $sql="select cid,cname,vcount from candidate";
@@ -9,10 +11,8 @@ while($row=mysqli_fetch_array($result))
 	$vcount=$row['vcount'];
 	echo "Candidate Id:".$cid. "  Candidate Name:".$cname. "  Vote Count:".$vcount."<br>";
 }
-
-
 ?>
-<center>
 <br><br>
 <input type=button value=Back onclick="location='admin.html'">
 </center>
+</div>
